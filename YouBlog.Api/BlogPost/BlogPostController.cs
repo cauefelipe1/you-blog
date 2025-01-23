e3f6cde460e5c0570155b06c9f7cd3ca1fb5de7c
Caue Carvalho <cauefelipe1@hotmail.com>
2025-01-23 20:32:59 -0300

    Controller docs

--- YouBlog.Api/BlogPost/BlogPostController.cs
+++ YouBlog.Api/BlogPost/BlogPostController.cs
@@ -8,7 +8,7 @@ namespace YouBlog.Api.BlogPost
     [ApiController]
     [Route("api/posts")]
     public class BlogPostController : ControllerBase
-    {   
+    {
         private readonly IBlogPostService _service;
         
         public BlogPostController(IBlogPostService service)
@@ -16,6 +16,10 @@ namespace YouBlog.Api.BlogPost
             _service = service;
         }
 
+        /// <summary>
+        /// Gets all blog posts.
+        /// </summary>
+        /// <returns>A list of blog posts.</returns>
         [HttpGet]
         [ProducesResponseType(typeof(IEnumerable<BlogPostModel>), 200)]
         public async Task<ActionResult<IEnumerable<BlogPostModel>>> GetAllPosts()
@@ -25,6 +29,11 @@ namespace YouBlog.Api.BlogPost
             return Ok(posts);
         }
 
+        /// <summary>
+        /// Creates a new blog post.
+        /// </summary>
+        /// <param name="newPostModel">The new blog post model.</param>
+        /// <returns>The created blog post.</returns>
         [HttpPost]
         [ProducesResponseType(typeof(BlogPostModel), 200)]
         [ProducesResponseType(400)]
@@ -34,13 +43,18 @@ namespace YouBlog.Api.BlogPost
                 return BadRequest();
 
             var createdPost = await _service.Create(newPostModel.ToModel());
-            
+
             return CreatedAtAction(
-                nameof(GetPostById), 
-                new { id = createdPost.Id }, 
+                nameof(GetPostById),
+                new { id = createdPost.Id },
                 createdPost);
         }
 
+        /// <summary>
+        /// Gets a blog post by its ID.
+        /// </summary>
+        /// <param name="id">The ID of the blog post.</param>
+        /// <returns>The blog post with the specified ID.</returns>
         [HttpGet("{id}")]
         [ProducesResponseType(typeof(BlogPostModel), 200)]
         [ProducesResponseType(400)]
@@ -51,13 +65,19 @@ namespace YouBlog.Api.BlogPost
                 return BadRequest();
 
             var post = await _service.GetById(id);
-            
+
             if (post is null)
                 return NotFound();
 
             return Ok(post);
         }
 
+        /// <summary>
+        /// Adds a comment to a blog post.
+        /// </summary>
+        /// <param name="blogpostId">The ID of the blog post.</param>
+        /// <param name="newComment">The new comment model.</param>
+        /// <returns>The added comment.</returns>
         [HttpPost("{id}/comments")]
         [ProducesResponseType(typeof(CommentModel), 200)]
         [ProducesResponseType(400)]
@@ -71,7 +91,7 @@ namespace YouBlog.Api.BlogPost
             try
             {
                 var comment = await _service.AddComment(blogpostId, newComment.ToModel());
-                
+
                 return CreatedAtAction(nameof(GetPostById), new { id = blogpostId }, comment);
             }
             catch (ArgumentException e)
